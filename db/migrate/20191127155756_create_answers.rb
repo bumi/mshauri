211ad1599387f5a1a7b47cc11d47763d044aef54
Michael Bumann <hello@michaelbumann.com>
2019-11-27 18:28:41 +0100

    Make sure to create the questions table first
    We need the table to be able to reference it from the answers table

--- db/migrate/20191127155756_create_answers.rb
+++ db/migrate/20191127155756_create_answers.rb
@@ -1,12 +1,12 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
+      t.references :next_question, null: false, foreign_key: { to_table: :questions }
+      t.references :question, null: false, foreign_key: true
+
       t.string :value
       t.boolean :freetext
       t.float :score
-      t.references :next_question, to_table: :questions, null: false, foreign_key: true
-      t.references :question, null: false, foreign_key: true
-
       t.timestamps
     end
   end
