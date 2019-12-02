d75b9c210e4aafcb85f2aaeee4bdee0d07391888
Lewis Dylan <lewisdylannkuranga@gmail.com>
2019-12-02 13:31:57 +0200

    Add css file to project

--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -3,6 +3,19 @@
         <div class="text-center">
             <h2>Hi {{user.name}}</h2>
             <h3>Start learning</h3>
+
+        </div>
+        <div class="flex">
+           <div class="w-50 center">
+               <div class="mt-4 b-2 bg-blue-darker w-25 h-100% center r-5">
+              Iteration
+               </div>
+           </div>
+            <div class="w-50">
+                <div class="mt-4 b-2 bg-grey-lighter w-25 h-100% center r-5">
+                   <i class="fas fa-times-circle"></i>
+                </div>
+            </div>
         </div>
     </div>
 </template>
