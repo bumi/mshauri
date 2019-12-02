ebe5fbbc20c699accbceb7f5c08a86f5825fbb97
yannick <musayann@gmail.com>
2019-12-02 11:08:01 +0200

    Rubocop cleanup

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 
 require 'faker'
 FactoryBot.define do
   factory :user do
-    name { "lewis"}
-    email { "lewisdylan@gmail.com" }
+    name { 'lewis' }
+    email { 'lewisdylan@gmail.com' }
   end
 end
--- spec/models/iteration_spec.rb
+++ spec/models/iteration_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Iteration, type: :model do
-  context "Create iteration" do
-    it 'should have user' do
-      user=FactoryBot.create(:user)
+  context 'when creating' do
+    it 'with user' do
+      user = FactoryBot.create(:user)
       expect(user.iterations.create).to be_valid
     end
   end
