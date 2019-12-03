6ab638b39446d08e3660b1c28bd9188f28a7f4ea
Michael Bumann <hello@michaelbumann.com>
2019-12-03 23:39:51 +0100

    no empty answers for now

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -8,7 +8,7 @@ class Answer < ApplicationRecord
   has_many :iteration_answers, dependent: :destroy
   has_many :iterations, through: :iteration_answers
 
-  #validates :value, presence: true
+  validates :value, presence: true
 
   def init
     self.score ||= 0.0
