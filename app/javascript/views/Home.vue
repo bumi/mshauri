376c1abb95da473f092c0b3577df5cc04e4ce007
yannick <musayann@gmail.com>
2019-12-03 12:03:42 +0200

    Assign question route iteration_id
    Set the iteration_id in the route to match the chosen iteration

--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -6,13 +6,15 @@
 
         </div>
         <div class="flex flex-wrap justify-content-center">
-            <div @click="newIteration" class="mt-3 b-2 w-25 border-2 border-solid mr-3 h-100 text-center p-5 bg-primary text-white cursor-pointer">
+            <div @click="newIteration"
+                 class="mt-3 b-2 w-25 border-2 border-solid mr-3 h-100 text-center p-5 bg-primary text-white cursor-pointer">
                 <div class="w-100 h-32 mx-auto flex flex-wrap align-items-center justify-content-center">
                     <p class="text-center text-6xl w-100 m-0"><i class="fas fa-plus-circle"></i></p>
                     <p class="text-center m-0"> New iteration</p>
                 </div>
             </div>
-            <div @click="openIteration(iteration.id)" class="mt-3 b-2 w-25 border-2 border-solid mr-3 h-100 text-center p-5 cursor-pointer"
+            <div @click="openIteration(iteration)"
+                 class="mt-3 b-2 w-25 border-2 border-solid mr-3 h-100 text-center p-5 cursor-pointer"
                  v-for="iteration  in iterations">
                 <div class="w-rem-32 h-32 mx-auto rounded-full border-2 border-solid flex align-items-center justify-content-center">
                     <p class="text-center text-3xl">{{iteration.completion_rate}} %</p>
@@ -49,11 +51,14 @@
                 })
             }, newIteration() {
                 Iteration.create(data => {
-                    this.openIteration(data.starting_question_id);
+                    this.openIteration(data);
                     console.log(data)
                 })
-            },openIteration(id){
-                this.$router.push({name: 'question', params: {id: id}});
+            }, openIteration(iteration) {
+                this.$router.push({
+                    name: 'question',
+                    params: {id: iteration.starting_question_id, iteration_id: iteration.id}
+                });
             }
         }, mounted() {
             this.getUser();
