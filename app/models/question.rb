185c0d07ca038f3a4992e9739d91084605529972
yannick <musayann@gmail.com>
2019-12-03 10:44:38 +0200

    Add answers in the question model json response

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,18 @@
 class Question < ApplicationRecord
   after_initialize :init
 
+  has_many :answers
+
   validates :title, presence: true
   validates :description, presence: false
 
   def init
     self.entry ||= false
+  end
+
+  def as_json(args = {})
+    super(args.merge(
+        methods: %i[answers]
+    ))
   end
 end
