d1db35792d83b4ecf575de28671d3a3cd4300afa
Michael Bumann <hello@michaelbumann.com>
2019-12-03 15:02:35 +0100

    Add next_question to the IterationAnswer response

--- app/models/iteration_answer.rb
+++ app/models/iteration_answer.rb
@@ -4,4 +4,14 @@ class IterationAnswer < ApplicationRecord
   belongs_to :answer
   belongs_to :iteration
   belongs_to :question
+
+  def next_question
+    self.answer.next_question
+  end
+
+  def as_json(args = {})
+    super(args.merge(
+      methods: %i[next_question]
+    ))
+  end
 end
