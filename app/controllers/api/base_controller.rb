c0e8488564dbedfb0c10164efe1d3328ceff7229
Lewis Dylan <lewisdylannkuranga@gmail.com>
2019-12-03 12:17:14 +0200

    Add option to retrieve iteration using iteration id
    Added option to retrieve iteration using iteration id

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -11,6 +11,8 @@ module Api
     # the current iteration is always the last iteration
     # When a user creates a new iterations the old ones can not be changed again
     def current_iteration
+      return @current_iteration ||= current_user.iterations.find(params[:iteration_id]) || current_user.iterations.last if params[:iteration_id]
+      ​
       @current_iteration ||= current_user.iterations.last
     end
 
