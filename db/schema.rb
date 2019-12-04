b7a73f010ddd248fc2bb5e90fd0a01ec6d53bf43
Michael Bumann <hello@michaelbumann.com>
2019-12-04 13:26:17 +0100

    Add options to recommendations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_180507) do
+ActiveRecord::Schema.define(version: 2019_12_04_100958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2019_12_02_180507) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
+    t.integer "priority"
     t.index ["answer_id"], name: "index_recommendations_on_answer_id"
   end
 
