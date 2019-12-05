16da72e69fa0dd64e9b07b85612f713dd39718df
yannick <musayann@gmail.com>
2019-12-05 12:31:38 +0200

    Add validators and index function in the recommendation controller

--- app/controllers/api/recommendations_controller.rb
+++ app/controllers/api/recommendations_controller.rb
@@ -2,6 +2,13 @@
 
 module Api
     class RecommendationsController < BaseController
+        before_action :require_current_user
+        before_action :require_current_iteration
+
+        def index
+            render json: current_iteration.recommendations.all
+        end
+
         def show
             @recommendation=Recommendation.find(params[:id])
             render json:@recommendation
