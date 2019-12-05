ed7e380df32ee0de341595da22045951d098c600
yannick <musayann@gmail.com>
2019-12-05 15:12:26 +0200

    Rubocop cleanup

--- app/controllers/api/recommendations_controller.rb
+++ app/controllers/api/recommendations_controller.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
 module Api
-    class RecommendationsController < BaseController
-        before_action :require_current_user
-        before_action :require_current_iteration
+  class RecommendationsController < BaseController
+    before_action :require_current_user
+    before_action :require_current_iteration
 
-        def index
-            render json: current_iteration.recommendations.all
-        end
-
-        def show
-            @recommendation=Recommendation.find(params[:id])
-            render json:@recommendation
-        end
-
+    def index
+      render json: current_iteration.recommendations.all
     end
+
+    def show
+      @recommendation = Recommendation.find(params[:id])
+      render json: @recommendation
+    end
+  end
 end
