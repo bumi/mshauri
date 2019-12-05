28023458d947deb282c688789401633483385963
Lewis Dylan <lewisdylannkuranga@gmail.com>
2019-12-05 16:40:36 +0200

    Make recommendation show component

---
+++ app/javascript/views/recommendations/Index.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+           <span  v-for="recommendation in recommendations" :key="recommendation.id">
+             <router-link :to="{name: 'recommendations-show', params:{id: recommendation.id}}">
+                {{recommendation.title}}
+             </router-link>
+           </span>
+        
+</div>
+</template>
+
+<script>
+    import Recommendation from "../../models/Recommendation"
+    export default {
+        name: 'RecommendationIndex',
+        data() {
+            return {
+                recommendations: [],
+            }
+        },
+    
+        mounted() {
+            Recommendation.index(this.$route.params.iteration_id, (data) => {
+                this.recommendations = data;
+                console.log(data)
+            });
+        }
+    }
+</script>
