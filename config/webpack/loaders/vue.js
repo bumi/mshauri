ce8d71c0bf786d6111ce224b2d9ff417a6698484
Manuel Wiedenmann <manuel@funkensturm.de>
2019-12-08 12:08:29 +0200

    Setup eslint

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -2,5 +2,13 @@ module.exports = {
   test: /\.vue(\.erb)?$/,
   use: [{
     loader: 'vue-loader'
-  }]
+  }],
+  rules: [
+    {
+      enforce: 'pre',
+      test: /\.(js|vue)$/,
+      loader: 'eslint-loader',
+      exclude: /node_modules/
+    }
+  ]
 }
