44744f3437643fbaeca65522e2c45eb35f2d6473
Lewis Dylan <lewisdylannkuranga@gmail.com>
2019-12-13 12:20:51 +0200

    Remove admin route

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
     resources :questions, only: [:show]
     resources :iterations, only: %i[index show create]
     resources :recommendations, only: %i[index show]
-    resources :admin, only: [:admin_dashboard]
   end
 
   # entry point for the vue.js
