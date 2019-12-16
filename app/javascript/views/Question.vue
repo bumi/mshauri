5aa3da237aea00a644020e845964b641e03c2330
Michael Bumann <hello@michaelbumann.com>
2019-12-17 00:13:59 +0100

    Remove home button on questions pages
    The user should stick to answering the questions.

--- app/javascript/views/Question.vue
+++ app/javascript/views/Question.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="w-90 md:w-100 xs:w-100 sm:w-100 mx-auto py-3 my-5 xs:my-0 bg-white rounded-lg">
     <div class="w-80 mx-auto">
-      <div class="text-left my-3">
-        <button
-
-          class="btn bg-grey-light rounded py-2 text-base"
-        >
-          Home
-        </button>
-      </div>
       <div class="w-100 mt-4">
         <div class="h-3 w100 bg-grey-lighter rounded-full">
           <div
