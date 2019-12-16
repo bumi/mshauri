0fa0ed924ed6740d1eee1b6996eb111b5c1e1b44
yannick <musayann@gmail.com>
2019-12-16 10:44:10 +0200

    Remove unnecessary changes in questions

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -2,15 +2,6 @@
 
 module Api
   class QuestionsController < BaseController
-    before_action :require_current_user, only: [:index]
-
-    def index
-      @user = current_user
-      authorize @user
-      questions = Question.all
-      render json: questions
-    end
-
     def show
       @question = Question.find(params[:id])
     end
