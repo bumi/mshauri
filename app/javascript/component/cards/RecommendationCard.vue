a57c5112b8475a75c7741fc032ae6f0bf48d0c0d
yannick <musayann@gmail.com>
2019-12-16 20:14:12 +0200

    Redesign the recommendations pages
    Redesigned the recommendations index page along with the recommendation
    card.
    
    Added link color styling to allow them to be shown differently in the
    recommendation show page

--- app/javascript/component/cards/RecommendationCard.vue
+++ app/javascript/component/cards/RecommendationCard.vue
@@ -1,34 +1,34 @@
 <template>
-  <div class="card card-default h-100 w-30 mx-2 my-3 shadow  rounded-t-lg">
-    <router-link
-      tag="div"
-      class="cursor-pointer"
-      :to="{name: 'recommendation-show', params:{id: recommendation.id, title: recommendation.title}}"
-    >
-      <div class="w-90 font-primary mx-auto">
-        <h5 class="font-bold text-base">
-          {{ recommendation.title }}
-        </h5>
-        <div class="my-3 text-left">
-          <div>
-            {{ recommendation.intro }}
-          </div>
+  <div class="w-80 mx-auto flex relative py-2">
+    <div class="absolute h-100 w-10 flex align-items-center justify-content-center">
+      <div class="h-100 w-05 bg-grey-light"></div>
+    </div>
+    <div class="w-10 z-99 flex align-items-center justify-content-center">
+      <div
+        class="rounded-full h-16 w-rem-16 bg-white border-solid border-1 border-grey-lighter shadow z-9999 flex align-items-center justify-content-center text-2xl">
+        <div class="rounded-full h-4 bg-grey-darker" style="width: 1rem">
         </div>
       </div>
-      <div class="text-right w-90  my-4 text-blue cursor-pointer">
-        <span class="text-sm">Read more</span>
-      </div>
-    </router-link>
+    </div>
+    <div class="w-90 py-5">
+      <router-link tag="div" class="cursor-pointer"
+        :to="{name: 'recommendation-show', params:{id: recommendation.id, title: recommendation.title}}">
+        <div class="w-95 ml-auto bg-transparent border-grey-light shadow-sm border-solid border-1 rounded-xl">
+          <h4 class="text-lg pt-4 pb-2 p-0 m-0 pl-5 font-bold">{{ recommendation.title }}</h4>
+            <p class=" text-sm p-0 m-0 pl-5 pb-4 font-roboto font-light"> {{ recommendation.intro }}</p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 <script>
-export default {
-  name: "RecommendationCard",
-  props: {
-    recommendation: {
-      type: Object,
-      default: () => {}
+  export default {
+    name: "RecommendationCard",
+    props: {
+      recommendation: {
+        type: Object,
+        default: () => {}
+      }
     }
   }
-}
 </script>
--- app/javascript/views/recommendations/Index.vue
+++ app/javascript/views/recommendations/Index.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="w-100 mx-auto bg-white min-h-screen -mt-8">
-    <div
-      class="w-70 mx-auto pt-5"
-    >
-      <h2 class="mt-0 mb-4 text-center text-weight-normal font-nunito text-grey-dark text-xl">
-        Some recommendations
-      </h2>
-    </div>
-    <div class="w-100 h-1 bg-primary" />
+    <div class="relative pt-5">
+     <div class="w-80 mx-auto  h-20 bg-transparent border-grey-light shadow-sm border-solid border-1 ">
+                <h1 class="text-primary text-2xl py-4 my-2 pl-5">Recommendations</h1>
+            </div>
+   </div>
     <div
       v-if="recommendations.length"
-      class="w-90 mx-auto pt-4 pb-5"
+      class="w-90 mx-auto pb-5"
     >
       <div class="flex flex-wrap">
         <recommendation-card
