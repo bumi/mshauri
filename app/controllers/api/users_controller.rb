bad872e934ef792055fc9f88515d41593169222a
yannick <musayann@gmail.com>
2019-12-17 11:55:33 +0200

    Fix authorize bug in user index
    Fixed a bug that was occurring because, we didn’t require
    the current user before index action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@
 
 module Api
   class UsersController < BaseController
-    before_action :require_current_user, only: %i[me show]
+    before_action :require_current_user, only: %i[index me show]
 
     def index
       authorize @current_user
