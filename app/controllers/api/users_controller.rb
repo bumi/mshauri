38c9478579f7ee2ec339d82505e724f4b40de684
Michael Bumann <hello@michaelbumann.com>
2019-12-17 16:49:45 +0100

    Cleanup
    We no longer use the api users controller for signups

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,19 +8,6 @@ module Api
       render json: current_user
     end
 
-    def create
-      @user = User.new(user_params)
-      if @user.save && @user.iterations.build.save # TODO: refactor iteration creation and redirect to iteration
-        redirect_to iteration_question_url(
-          user_slug:    @user.slug,
-          iteration_id: @user.iterations.last.id,
-          question_id:  @user.iterations.last.starting_question_id
-        )
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
-
     private
 
     def user_params
