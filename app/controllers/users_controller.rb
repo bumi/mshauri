14dabfa88d6d4e079f16d1c810c4b95072984666
Michael Bumann <hello@michaelbumann.com>
2019-12-17 16:38:12 +0100

    Add login option
    If the user is already registered we send a login email to the user with
    the private link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,21 @@
 
 class UsersController < ApplicationController
   def create
+    login or register
+  end
+
+  private
+
+  def login
+    @user = User.find_by(email: user_params[:email])
+    return false unless @user
+
+    UserMailer.with(user: @user).login.deliver_now
+    flash[:notice] = 'Welcome back. Please use the link in the email to access your recommendations'
+    redirect_to root_url
+  end
+
+  def register
     @user = User.new(user_params)
     if @user.save && @user.iterations.build.save # TODO: refactor iteration creation and redirect to iteration
       redirect_to iteration_question_url(
@@ -13,8 +28,6 @@ class UsersController < ApplicationController
       render '/home/welcome'
     end
   end
-
-  private
 
   def user_params
     params.require(:user).permit(:email, :name)
