fda041435bd965dda19540a2364dcaca97d20291
Michael Bumann <hello@michaelbumann.com>
2019-12-17 09:46:10 +0100

    Correct email subject and sender

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  default from: 'notifications@mshauri.com'
+  default from: 'contact@tech-guide.rw'
 
   def welcome_email
     @user = params[:user]
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Welcome to the Rwanda Tech Guide')
   end
 end
