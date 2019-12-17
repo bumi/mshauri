603c0ceaf8b2c3d865696846a98e0596a9e9cae7
Michael Bumann <hello@michaelbumann.com>
2019-12-17 08:42:50 +0100

    Correct staring question

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,7 +11,7 @@ module Api
     def create
       user = User.new(user_params)
       if user.save && user.iterations.build.save # TODO: refactor iteration creation and redirect to iteration
-        redirect_to iteration_question_url(user_slug: user.slug, iteration_id: user.iterations.last.id, question_id: Question.first.id)
+        redirect_to iteration_question_url(user_slug: user.slug, iteration_id: user.iterations.last.id, question_id: user.iterations.last.starting_question_id)
       else
 
         render json: user.errors, status: :unprocessable_entity
