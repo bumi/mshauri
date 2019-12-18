b40ccb165991ab19bfe3d5b9d75551c385fb74a5
Michael Bumann <hello@michaelbumann.com>
2019-12-18 17:21:41 +0100

    Extend content with getting started hints

---
+++ db/seeds/0_recommendations.rb
@@ -0,0 +1,13 @@
+Dir['./data/recommendations/*.yml'].each do |filename|
+  identifier = File.basename(filename, '.yml')
+  puts "Creating recommendation #{identifier}"
+  recommendation_data = YAML.safe_load(File.read(filename))
+  next if recommendation_data.nil?
+
+  recommendation = Recommendation.where(identifier: identifier).first_or_initialize
+  recommendation.title = recommendation_data['title']
+  recommendation.intro = recommendation_data['intro']
+  recommendation.description = recommendation_data['description']
+  recommendation.priority = recommendation_data['priority'] || 0
+  recommendation.save!
+end
