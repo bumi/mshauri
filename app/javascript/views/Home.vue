7560b5f049dee1bf95a19c62ba7d5b464ab12246
yannick <musayann@gmail.com>
2019-12-18 17:31:38 +0200

    Mobile responsiveness fixes

--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -1,39 +1,41 @@
 <template>
-  <div class="bg-white py-2 pb-5 my-5 w-90 mx-auto rounded-xl">
-    <div class="w-80 mx-auto xs:mx-auto py-4 flex xs:block sm:w-85">
-      <div class="w-80 xs:w-100 ">
-        <h5 class="my-0">
-          Hey, You Can Start Or Resume Surveys
-        </h5>
-        <span class="text-xs text-grey-dark">Complete surveys to view recommendations</span>
-      </div>
+  <div class="xs:py-1">
+    <div class="bg-white py-2 pb-5 my-5 w-90 mx-auto rounded-xl">
+      <div class="w-80 mx-auto xs:mx-auto py-4 flex xs:block sm:w-85">
+        <div class="w-80 xs:w-100 ">
+          <h5 class="my-0">
+            Hey, You Can Start Or Resume Surveys
+          </h5>
+          <span class="text-xs text-grey-dark">Complete surveys to view recommendations</span>
+        </div>
 
-      <div
-        class="w-20 xs:w-100 sm:w-40 md:w-40 xs:mt-3"
-        @click="newIteration"
-      >
-        <button class=" btn btn-primary rounded-lg py-2 text-base cursor-pointer w-100 xs:text-2xl shadow-sm cursor-pointer">
-          Start New Iteration
-        </button>
+        <div
+          class="w-20 xs:w-100 sm:w-40 md:w-40 xs:mt-3"
+          @click="newIteration"
+        >
+          <button class=" btn btn-primary rounded-lg py-2 text-base cursor-pointer w-100  shadow-sm cursor-pointer">
+            Start New Iteration
+          </button>
+        </div>
       </div>
-    </div>
-    <div
-      class="w-100 bg-grey-lighter"
-      style="height:2px"
-    />
-
-    <div class="w-90 xs:w-95 text-left ml-auto">
-      <h6 class="my-4 text-xl font-semibold">
-        Recent iteration
-      </h6>
-    </div>
-    <div class="flex flex-wrap xs:block xs:w-90 xs:mx-auto justify-content-center md:w-90 md:mx-auto py-4">
       <div
-        v-for="iteration in iterations"
-        :key="iteration.id"
-        class="mt-3 w-25 xs:w-100 md:w-40 sm:w-40 mr-3 mx-3 xs:mx-0 xs:mr-0"
-      >
-        <iteration-card :iteration="iteration" />
+        class="w-100 bg-grey-lighter"
+        style="height:2px"
+      />
+
+      <div class="w-90 xs:w-95 text-left ml-auto">
+        <h6 class="my-4 text-xl font-semibold">
+          Recent iterations
+        </h6>
+      </div>
+      <div class="flex flex-wrap xs:block xs:w-90 xs:mx-auto justify-content-center md:w-90 md:mx-auto py-4">
+        <div
+          v-for="iteration in iterations"
+          :key="iteration.id"
+          class="mt-3 w-25 xs:w-100 md:w-40 sm:w-40 mr-3 mx-3 xs:mx-0 xs:mr-0"
+        >
+          <iteration-card :iteration="iteration" />
+        </div>
       </div>
     </div>
   </div>
@@ -66,17 +68,23 @@ export default {
   },
   methods: {
     getUser() {
-      User.show('me').then(({ data }) => {
+      User.show('me').then(({
+        data
+      }) => {
         this.user = data
       });
     },
     getIterations() {
-      Iteration.index().then(({ data }) => {
+      Iteration.index().then(({
+        data
+      }) => {
         this.iterations = data
       })
     },
     newIteration() {
-      Iteration.create().then(({ data }) => {
+      Iteration.create().then(({
+        data
+      }) => {
         this.openIteration(data);
         console.log(data)
       })
