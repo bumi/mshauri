f4c033a092d22000ba0a7100b092ef09296c32d6
Michael Bumann <hello@michaelbumann.com>
2019-12-18 15:21:54 +0100

    fix linting

--- app/javascript/routes/web.js
+++ app/javascript/routes/web.js
@@ -6,11 +6,12 @@ import RecommendationShow from "../views/recommendations/Show"
 
 export default {
   mode: "history",
-  scrollBehavior (to, from, savedPosition) {
+  scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition
     } else {
-      return { x: 0, y: 0 }
+      return { x: 0,
+        y: 0 }
     }
   },
   routes: [
