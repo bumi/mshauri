8b9ae0bb83bfbe09fc1266c509316ac6053d97d6
Manuel Wiedenmann <manuel@funkensturm.de>
2019-12-18 16:22:58 +0100

    Skip pundit for questions

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -2,8 +2,17 @@
 
 module Api
   class QuestionsController < BaseController
+    # TODO: Maybe add a policy
+    skip_after_action :verify_authorized, only: %i[show]
+    skip_after_action :verify_policy_scoped, only: %i[index]
+
     def show
       @question = Question.find(params[:id])
+    end
+
+    def index
+      questions = Question.all
+      render json: questions
     end
   end
 end
