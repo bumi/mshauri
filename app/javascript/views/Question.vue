74ca6cb218008a2c98f25abb59388895a2e6fb9e
Michael Bumann <hello@michaelbumann.com>
2019-12-18 14:09:48 +0100

    Add link to recommendations from the question page
    This allows the user to see the recommendations before she finished the
    questionnaire.
    
    This is especially helpful to show it to people that they get an idea of
    what they will get as recommendations.

--- app/javascript/views/Question.vue
+++ app/javascript/views/Question.vue
@@ -56,6 +56,15 @@
             Next
           </button>
         </div>
+        <div class="text-right xs:block mt-2 text-grey-dark">
+          <router-link
+            tag="div"
+            class="cursor-pointer"
+            :to="{name: 'recommendations-index', params:{iteration_id: iteration.id}}"
+          >
+          See recommendations
+          </router-link>
+        </div>
       </div>
       <div
         v-if="loading"
