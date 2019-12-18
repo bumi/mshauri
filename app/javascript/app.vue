acd43885bd89b1fbc2b9c93e714bd131a4096dd8
Arsene Karema <karema@octangrp.com>
2019-12-18 15:56:36 +0200

    Change issues in responsiveness
    Remove the margin top of the question card.

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,7 +3,7 @@
     id="app"
     class="bg-grey-lighter min-h-screen"
   >
-    <div class="w-80 xs:w-100 mx-auto py-4">
+    <div class="w-80 xs:w-100 mx-auto py-4 xs:py-0">
       <router-view />
     </div>
   </div>
