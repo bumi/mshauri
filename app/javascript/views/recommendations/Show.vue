29a582558c9fc8862b70047e06c381635f65fb21
yannick <musayann@gmail.com>
2019-12-18 13:55:32 +0200

    Redesign breadcrumb

--- app/javascript/views/recommendations/Show.vue
+++ app/javascript/views/recommendations/Show.vue
@@ -1,49 +1,49 @@
 <template>
   <!-- eslint-disable vue/no-v-html -->
   <div class="w-100 my-4">
-    <div class="w-85">
-      <div class="p-3 bg-grey-light w-auto inline-block rounded-xl mb-5">
-        <p class="text-xs my-0">
-          <!-- TODO: update link to recommendations index -->
-          <span class="text-accent cursor-pointer" @click="goBack">
-            Recommendations <i class="fas fa-chevron-right mx-1" />
-          </span>
-          <span>{{ recommendation.title }}</span>
-        </p>
-      </div>
-      <div class="mx-auto bg-white min-h-screen overflow-hidden my-4 rounded-lg py-5">
-        <div class="relative pt-4">
+    <div class="w-85 mx-auto">
+      <div class="bg-white min-h-screen overflow-hidden my-4 rounded-lg py-5">
+        <div class="relative pt-4 z-10">
           <div
-            class="w-85 mx-auto  h-20 bg-transparent border-grey-light shadow-sm border-solid border-1 overflow-hidden">
+            class="w-85 mx-auto  h-20 bg-transparent border-grey-light shadow-sm border-solid border-1 overflow-hidden"
+          >
             <h1
-              class="text-primary text-2xl  my-4 pl-5 xs:pl-0 xs:px-3 xs:text-xl xs:pt-2 md:text-xl md:pl-0 md:px-5 md:pt-2">
-              {{ recommendation.title }}
+              class="text-primary text-2xl  my-4 pl-5 xs:pl-0 xs:px-3 xs:text-xl xs:pt-2 md:text-xl md:pl-0 md:px-5 md:pt-2"
+            >
+              <span
+                class="text-grey-dark text-base cursor-pointer"
+                @click="goBack"
+              >Recommendation<i class="fas fa-chevron-right mx-3" /></span>{{ recommendation.title }}
             </h1>
           </div>
         </div>
 
-        <div class="w-80 mx-auto flex relative py-5 -mt-12">
+        <div class="w-80 mx-auto flex relative py-5 -mt-12 z-0">
           <div class="absolute h-100 w-10 flex align-items-center justify-content-center">
             <div class="h-100 w-05 bg-grey-light" />
           </div>
         </div>
         <div class="flex flex-wrap pb-4">
-
           <div class="w-80 mx-auto flex relative py-2">
             <div class="absolute h-100 w-10 flex align-items-center justify-content-center">
               <div class="h-100 w-05 bg-grey-light" />
             </div>
             <div class="w-10 z-99 flex align-items-center justify-content-center">
               <div
-                class="rounded-full h-16 w-rem-16 xs:h-8 xs:w-rem-8 md:h-12 md:w-rem-12 sm:h-10 sm:w-rem-10 bg-white border-solid border-1 border-grey-lighter shadow z-9999 flex align-items-center justify-content-center text-2xl">
-                <div class="rounded-full h-4 bg-grey-darker" style="width: 1rem" />
+                class="rounded-full h-16 w-rem-16 xs:h-8 xs:w-rem-8 md:h-12 md:w-rem-12 sm:h-10 sm:w-rem-10 bg-white border-solid border-1 border-grey-lighter shadow z-9999 flex align-items-center justify-content-center text-2xl"
+              >
+                <div
+                  class="rounded-full h-4 bg-grey-darker"
+                  style="width: 1rem"
+                />
               </div>
             </div>
             <div class="w-90 py-3">
               <div class="w-95 ml-auto bg-transparent border-grey-light shadow-sm border-solid border-1 rounded-xl">
                 <div
                   class="text-sm p-0 m-0 px-5 py-4 xs:pl-0 xs:px-3 md:pl-0 md:px-4 sm:pl-0 sm:px-4 font-roboto font-light"
-                  v-html="recommendation.description_formatted" />
+                  v-html="recommendation.description_formatted"
+                />
               </div>
             </div>
           </div>
@@ -54,25 +54,25 @@
 </template>
 
 <script>
-  import Recommendation from "../../models/Recommendation"
-  export default {
-    name: 'RecommendationShow',
-    data() {
-      return {
-        recommendation: {}
-      }
-    },
-    mounted() {
-      Recommendation.show(this.$route.params.id).then(({
-        data
-      }) => {
-        this.recommendation = data;
-      });
-    },
-    methods: {
-      goBack() {
-        return window.history.back();
-      }
-    },
-  }
+import Recommendation from "../../models/Recommendation"
+export default {
+  name: 'RecommendationShow',
+  data() {
+    return {
+      recommendation: {}
+    }
+  },
+  mounted() {
+    Recommendation.show(this.$route.params.id).then(({
+      data
+    }) => {
+      this.recommendation = data;
+    });
+  },
+  methods: {
+    goBack() {
+      return window.history.back();
+    }
+  },
+}
 </script>
