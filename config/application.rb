b0c19827b174d2725ced36b50fb257bb5609bef2
Michael Bumann <hello@michaelbumann.com>
2019-12-20 10:55:22 +0100

    Fix email deliver and make sender configurable

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,5 @@ module Mshauri
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] || 'localhost:3000' }
   end
 end
