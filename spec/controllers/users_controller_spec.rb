a112be79f04c2c986ec13e6ade3a3c70d135ab87
Lewis Dylan <lewisdylannkuranga@gmail.com>
2020-01-15 15:55:15 +0200

    Add user's and question's controller test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UsersController do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe 'GET #index' do
+    it 'renders the :index view' do
+      get :index, params: { user_id: user.id }
+      #   response.should render_template :index
+      response.response_code.should == 200
+    end
+
+    it 'renders the #show view' do
+      @user = FactoryBot.create(:user)
+      #   get :show, params: { id: FactoryBot.create(:user) }
+      #   response.should render_template :show
+      get :show, params: { id: @user.to_param, template: 'users/show' }
+      expect(response).to render_template :show
+      response.response_code.should == 200
+    end
+  end
+end
