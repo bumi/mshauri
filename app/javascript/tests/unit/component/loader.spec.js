df60a4054edc268f0966c2ecec7971d114c6202c
musayann <musayann@gmail.com>
2020-01-15 11:50:00 +0200

    Wrap up the loader component test

--- app/javascript/tests/unit/component/loader.spec.js
+++ app/javascript/tests/unit/component/loader.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import Loader from '@/component/Loader'
+import Vue from 'vue'
 
 describe('Loader', () => {
   it('renders invisible when the value is false', () => {
@@ -9,12 +10,22 @@ describe('Loader', () => {
     expect(wrapper.vm.visible).toEqual(false)
   });
 
-  it('renders delayed visible when the value is true', async() => {
+  it('delays visibility when the props.value is true', async() => {
     const wrapper = shallowMount(Loader, {
       propsData: { value: true }
     });
     expect(wrapper.vm.visible).toEqual(false);
     await new Promise((r) => setTimeout(r, 500));
     expect(wrapper.vm.visible).toEqual(true);
+  });
+
+  it('becomes invisible when the props.value is set to false', async() => {
+    const wrapper = shallowMount(Loader, {
+      propsData: { value: true }
+    });
+    await new Promise((r) => setTimeout(r, 500));
+    wrapper.setProps({ value: false })
+    await Vue.nextTick()
+    expect(wrapper.vm.visible).toEqual(false);
   });
 })
