1222d4181574ebb25120cb316dfebbeef44b3b54
Lewis Dylan <lewisdylannkuranga@gmail.com>
2020-01-15 09:33:30 +0200

    Add tests in the user model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -9,10 +9,18 @@ RSpec.describe Question, type: :model do
   #   it { should_not validate_presence_of(:description) }
   #
   # end
+  context 'with associations' do
+    it { is_expected.to have_many(:answers) }
+  end
+
+  context 'with validations' do
+    record = described_class.create()
+    record.valid? # run validations
+  end
 
   context 'when creating a question' do
     it 'has all the validations' do
-      expect(described_class.create(title: 'new', description: 'do u use any IDE')).to be_valid
+      expect(described_class.create(title: 'new', description: 'do you use any IDE')).to be_valid
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,23 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_many(:iterations) }
   end
 
-  context 'with alidations' do
+  context 'with validations' do
     # Basic validations
     it { is_expected.to validate_uniqueness_of(:slug) }
     # Inclusion/acceptance of values
+  end
+
+  context 'when creating user' do
+    it 'should be valid' do
+      user = FactoryBot.create(:user, name: 'name')
+      expect(user.name).to eq 'name'
+    end
+  end
+
+  context 'when creating a user' do
+    it ' user slug is valid' do
+      user = FactoryBot.create(:user, name: 'name', email: 'name@gmail.com')
+      expect(user.slug).should_not be_nil
+    end
   end
 end
