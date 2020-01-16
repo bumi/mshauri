b31308ebf45c92fe538f3b7b54592555b28ea78e
musayann <musayann@gmail.com>
2020-01-16 15:50:35 +0200

    Add get request format paramater

--- spec/controllers/api/iterations_controller_spec.rb
+++ spec/controllers/api/iterations_controller_spec.rb
@@ -5,13 +5,9 @@ require 'rails_helper'
 RSpec.describe Api::IterationsController, type: :controller do
   let(:user) { FactoryBot.create(:user) }
 
-  before do
-    request.env['HTTP_ACCEPT'] = 'application/json'
-  end
-
   describe 'GET' do
     it 'renders the #index view' do
-      get :index, params: { user_slug: user.slug }
+      get :index, params: { user_slug: user.slug }, format: :json
 
       expect(response.response_code).to be(200)
       # response.should render_template :show
@@ -19,7 +15,7 @@ RSpec.describe Api::IterationsController, type: :controller do
 
     it 'renders the #show view' do
       iteration = user.iterations.create
-      get :show, id: iteration, params: { user_slug: user.slug }
+      get :show, id: iteration, params: { user_slug: user.slug }, format: :json
 
       expect(response.response_code).to be(200)
       # response.should render_template :show
