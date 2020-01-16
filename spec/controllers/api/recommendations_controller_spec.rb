2ecff0a3498781fac87caa5c069d1a6b1b74c7be
Lewis Dylan <lewisdylannkuranga@gmail.com>
2020-01-16 15:52:39 +0200

    Fix recommendation show function test

--- spec/controllers/api/recommendations_controller_spec.rb
+++ spec/controllers/api/recommendations_controller_spec.rb
@@ -6,20 +6,21 @@ describe Api::RecommendationsController, type: :controller do
   # describe Api::RecommendationsController do
   let(:user) { FactoryBot.create(:user) }
 
-  # describe 'GET #index' do
-  #   it 'renders the :index view' do
-  #     user = FactoryBot.create(:user)
-  #     get :show, params: { user_slug: user.slug }
-  #     # expect(response.status).to eq(200)
-  #     # response.should render_template :index
-  #   end
-  # end
+  describe 'GET #all' do
+    it 'renders the :index view' do
+      user = FactoryBot.create(:user)
+      get :all, params: { user_slug: user.slug }, format: :json
+      # expect(response.status).to eq(200)
+      # response.should render_template :index
+    end
+  end
 
   describe 'GET' do
     it 'renders the #show view' do
       recommendation = FactoryBot.create(:recommendation)
-      get :show, params: { id: recommendation.id }
-      response.should render_template :show
+      get :show, params: { id: recommendation.id }, format: :json
+      # response.should render_template :show
+      assert_response :success
     end
   end
 end
