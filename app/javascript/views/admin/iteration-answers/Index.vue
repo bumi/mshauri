b878f7979a0826901567416349a70f52c8c7d4a4
musayann <musayann@gmail.com>
2020-01-20 11:15:21 +0200

    Add iteration answers empty state

--- app/javascript/views/admin/iteration-answers/Index.vue
+++ app/javascript/views/admin/iteration-answers/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-100 md:w-100 xs:w-100 sm:w-100 mx-auto py-3 my-3 xs:my-0 bg-white rounded-xl">
+    <loader :value="loading" />
     <div class="w-80 mx-auto">
       <div class="my-5">
         <div
@@ -46,6 +47,36 @@
             </div>
           </div>
         </div>
+        <div
+          v-if="iterationAnswers.length < 1 && !loading"
+          class="p-5"
+        >
+          <div class="w-100 p-5 opacity-60">
+            <div class="w-100 flex px-4 py-2">
+              <div class="w-auto pr-3">
+                <p class="my-0 leading-none text-4xl font-primary">
+                  Q:
+                </p>
+              </div>
+              <div class="w-90 ">
+                <div class="w-100 h-4 bg-black mt-1 rounded-full" />
+                <div class="w-80 h-3 bg-black mt-2 rounded-full" />
+                <div class="w-30 h-3 bg-black mt-2 rounded-full" />
+              </div>
+            </div>
+            <div class="w-100 flex px-4 py-2 text-primary-darkest py-4">
+              <div class="w-auto pr-3">
+                <p class="my-0 leading-none text-4xl font-primary">
+                  A:
+                </p>
+              </div>
+              <div class="w-90">
+                <div class="w-100 h-4 bg-black mt-2 rounded-full" />
+              </div>
+            </div>
+            <h3 class="text-center">No Question answered yet</h3>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -53,12 +84,17 @@
 <script>
 import IterationAnswer from "../../../models/IterationAnswer"
 import Iteration from "../../../models/Iteration"
+import Loader from "../../../component/Loader"
 export default {
   name: 'IterationAnswersIndex',
+  components: {
+    Loader
+  },
   data() {
     return {
       iterationAnswers: [],
-      iteration: {}
+      iteration: {},
+      loading: true
     }
   },
   mounted() {
@@ -66,7 +102,7 @@ export default {
       'iteration_id': this.$route.params.iteration_id
     }).then(({ data }) => {
       this.iterationAnswers = data;
-      console.log(data);
+      this.loading = false;
     });
     Iteration.show(this.$route.params.iteration_id).then(({ data }) => {
       this.iteration = data;
