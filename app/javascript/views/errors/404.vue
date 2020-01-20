558d3b615285f8ebbcfddda8aefa3ccd0d474e1c
musayann <musayann@gmail.com>
2020-01-20 18:21:38 +0200

    Implement the 404 page design

---
+++ app/javascript/views/errors/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-screen flex align-items-center justify-content-center -mt-4 -mb-4 sm:m-0 md:m-0">
+    <div class="w-50 xs:w-100 md:w-70 sm:w-60 sm:w-60">
+      <div class="w-100 xs:w-60 sm:w-30 xs:mx-auto sm:ml-auto text-right sm:text-right xs:text-center">
+        <img src="/assets/oops.png">
+      </div>
+      <div class="w-70 my-4 bg-white shadow-md px-4 py-5 rounded-xl xs:mx-auto">
+        <p class="m-0 text-black-dark">
+          404
+        </p>
+        <h3 class="font-primary font-semibold my-2 text-black-dark">
+          Page not found
+        </h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error404"
+}
+</script>
