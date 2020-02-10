f9bbd0ecf5080b51f451c2c535a45d135bf5046b
Michael Bumann <hello@michaelbumann.com>
2020-02-10 11:56:41 +0100

    cleanup

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -3,7 +3,7 @@
 user = User.where(email: 'admin@tech-guide.rw').first_or_initialize(name: 'admin')
 user.admin = true
 user.save!
-puts "-------"
+puts '------'
 puts "Created an admin user: admin@tech-guide.rw - slug: #{user.slug} (http://localhost:3000/#{user.slug})"
-puts "-------"
+puts '------'
 Iteration.where(user: user).first_or_create!
